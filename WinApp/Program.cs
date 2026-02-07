941db46639c73b8f3bcb0723ca3b95180ef10bdc
thinh <thinh@FVN-IT-THINH>
2026-02-07 10:28:19 +0700

    sdsdff

--- WinApp/Program.cs
+++ WinApp/Program.cs
@@ -21,31 +21,16 @@ namespace WinApp
             ConfigureServices();
 
             var authenExists = AuthenInfo();
-            /* if (authenExists == null)
-             {
-                 var mainForm = ServiceProvider!.GetRequiredService<LoginForm>();
-                 Application.Run(mainForm);
-             }
-             else
-             {
-                 var mainForm = ServiceProvider!.GetRequiredService<FormBanVe>();
-                 Application.Run(mainForm);
-             }*/
 
             if (authenExists == null)
             {
-
               var mainForm = ServiceProvider!.GetRequiredService<LoginForm>();
-                var mainForm = ServiceProvider!.GetRequiredService<FormBanVe>();
-
-                
 
                 Application.Run(mainForm);
             }
             else
             {
-              //  var mainForm = ServiceProvider!.GetRequiredService<FormBanVe>();
-                var mainForm = ServiceProvider!.GetRequiredService<FormTest>();
+                var mainForm = ServiceProvider!.GetRequiredService<FormBanVe>();
                 Application.Run(mainForm);
             }
         }
